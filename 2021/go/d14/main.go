1549345310d8f1574867a2378aa74edc8f6f52ad
Pekka Vainio <pekka.vainio@iki.fi>
2021-12-14 15:45:52 +0200

    Day 14 refactored

--- 2021/go/d14/main.go
+++ 2021/go/d14/main.go
@@ -7,43 +7,42 @@ import (
 	"strings"
 )
 
-var existingCounts = make(map[string]map[byte]uint64)
-var pairToCharRules = make(map[string]byte)
-
 // Advent of Code (AOC) 2021 Day 14
 func main() {
 
 	var template string
+	var pairToCharRules = make(map[string]string)
 
 	util.ReadFile("../input/14a.txt", func(line string) {
 		if strings.Contains(line, " -> ") {
 			parts := strings.Split(line, " -> ")
-			pairToCharRules[parts[0]] = parts[1][0]
+			pairToCharRules[parts[0]] = parts[1]
 		} else if line != "" {
 			template = line
 		}
 	})
 
-	part1 := solve(template, 10)
+	part1 := solve(template, 10, pairToCharRules)
 	fmt.Printf("part 1: %d\n", part1)
 
-	part2 := solve(template, 40)
+	part2 := solve(template, 40, pairToCharRules)
 	fmt.Printf("part 2: %d\n", part2)
 }
 
-func solve(template string, depth int) uint64 {
-	result := make(map[byte]uint64)
-	pairs := stringAsCharPairs(template)
-	for pos, pair := range pairs { // Loop template as pairs NNCB -> NN,NC,CB
-		lastPair := pos == len(pairs)-1 // Identify last pair for calculation
-		charCountsForPair := counCharsForPair(pair, lastPair, depth)
-		result = mergeCountsByChar(result, charCountsForPair)
+func solve(template string, steps int, pairToCharRules map[string]string) uint64 {
+	pairCounts := countPairs(stringAsPairs(template))
+	for i := 0; i < steps; i++ {
+		pairCounts = doStep(pairCounts, pairToCharRules)
 	}
+
+	charCounts := countFirstChars(pairCounts)
+	// since the above does not include last char of the template, add it
+	charCounts[template[len(template)-1]]++
 
 	// Calculate and return the difference between max and min char counts
 	var min uint64 = math.MaxUint64
 	var max uint64 = 0
-	for _, v := range result {
+	for _, v := range charCounts {
 		if v < min {
 			min = v
 		}
@@ -54,55 +53,43 @@ func solve(template string, depth int) uint64 {
 	return max - min
 }
 
-func counCharsForPair(pair []byte, lastPair bool, depth int) map[byte]uint64 {
-	if depth == 0 {
-		if lastPair {
-			return map[byte]uint64{pair[0]: 1, pair[1]: 1} // For last pair count both chars
-		} else {
-			return map[byte]uint64{pair[0]: 1} // For others only the first char count
-		}
+func doStep(pairs map[string]uint64, pairToCharRules map[string]string) map[string]uint64 {
+	pairCounts := make(map[string]uint64)
+	for pair, count := range pairs {
+		// explode pair to two pairs and add those counts to result
+		pairToChar := pairToCharRules[pair]
+		pair1 := string(pair[0]) + pairToChar
+		pair2 := pairToChar + string(pair[1])
+		pairCounts[pair1] += count
+		pairCounts[pair2] += count
 	}
-	key := fmt.Sprintf("%v%v%v", string(pair), lastPair, depth)
-	if res, ok := existingCounts[key]; ok {
-		return res // count calculated already, use it
-	} else {
-		// no existing counts calculation, calculate counts recursively
-		pairToChar := pairToCharRules[string(pair)]
-		pair1 := []byte{pair[0], pairToChar}
-		pair2 := []byte{pairToChar, pair[1]}
-		p1Count := counCharsForPair(pair1, false, depth-1)
-		p2Count := counCharsForPair(pair2, lastPair, depth-1)
-		res := mergeCountsByChar(p1Count, p2Count)
-		existingCounts[key] = res // store counts for later use
-		return res
-	}
+	return pairCounts
 }
 
-func mergeCountsByChar(a map[byte]uint64, b map[byte]uint64) map[byte]uint64 {
-	res := make(map[byte]uint64)
-	addCounts(res, a)
-	addCounts(res, b)
-	return res
-}
-
-func addCounts(res map[byte]uint64, source map[byte]uint64) {
-	for k, v := range source {
-		if existing, ok := res[k]; ok {
-			res[k] = existing + v
-		} else {
-			res[k] = v
-		}
+func countFirstChars(pairs map[string]uint64) map[byte]uint64 {
+	charCounts := make(map[byte]uint64)
+	for pair, count := range pairs {
+		charCounts[pair[0]] += count
 	}
+	return charCounts
 }
 
-func stringAsCharPairs(str string) [][]byte {
-	var prev byte
-	var result [][]byte
+func countPairs(pairs []string) map[string]uint64 {
+	pairCount := make(map[string]uint64)
+	for _, pair := range pairs {
+		pairCount[pair]++
+	}
+	return pairCount
+}
+
+func stringAsPairs(str string) []string {
+	var prev rune
+	var pairs []string
 	for _, c := range str {
 		if prev != 0 {
-			result = append(result, []byte{prev, byte(c)})
+			pairs = append(pairs, string([]rune{prev, c}))
 		}
-		prev = byte(c)
+		prev = c
 	}
-	return result
+	return pairs
 }
--- 2021/java/src/main/java/aoc2021/D14.java
+++ 2021/java/src/main/java/aoc2021/D14.java
@@ -4,78 +4,80 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
-import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
  * Advent of Code (AOC) 2021 Day 14
  */
- public class D14 {
+public class D14 {
 
-    static Map<String,Map<Character,Long>> cache = new ConcurrentSkipListMap<>();
-    static Map<String,Character> pairToCharacterRule;
-
-    public static void main(String ... args) throws IOException {
+    public static void main(String... args) throws IOException {
         List<String> data = Files.lines(Path.of("../input/14a.txt")).toList();
         String template = data.get(0);
-        pairToCharacterRule = data.stream().skip(2)
-            .map(l -> l.split(" -> "))
-            .collect(Collectors.toMap(l -> l[0], l -> l[1].charAt(0)));
-        
-        var part1 = solve(template, 10);
+
+        Map<String, String> pairToCharRules = data.stream().skip(2)
+                .map(l -> l.split(" -> "))
+                .collect(Collectors.toMap(l -> l[0], l -> l[1]));
+
+        var part1 = solve(template, 10, pairToCharRules);
         System.out.printf("part 1: %d\n", part1);
 
-        var part2 = solve(template, 40);
+        var part2 = solve(template, 40, pairToCharRules);
         System.out.printf("part 2: %d\n", part2);
     }
 
-    static long solve(String template, int depth) {
-        Map<Character,Long> result = new TreeMap<>();
-        Iterator<char[]> iter = stringAsCharPairs(template).iterator();
-        while (iter.hasNext()) { // Iterate template in pairs NNCB -> NN,NC,CB
-            char[] pair = iter.next();
-            boolean lastPair = !iter.hasNext(); // Identify last pair for calculation
-            var countForPair = countCharsForPair(pair, lastPair, depth);
-            result = mergeCountsByCharacter(result, countForPair);
+    static long solve(String template, int steps, Map<String, String> pairToCharRules) {
+        Map<String, Long> pairs = countPairs(template);
+
+        for (int s = 0; s < steps; s++) {
+            pairs = doStep(pairs, pairToCharRules);
         }
+
+        Map<Character, Long> sumByChar = countFirstCharsOfPairs(pairs);
+        // Have to add last char of template, since not included in the sumByChar sum
+        sumByChar.merge(template.charAt(template.length() - 1), 1l, (a, b) -> a + b);
+
         // Calculate and return the difference between max and min char counts
-        long max = result.values().stream().mapToLong(i -> i).max().getAsLong();
-        long min = result.values().stream().mapToLong(i -> i).min().getAsLong();
-        return max-min;
+        long max = sumByChar.values().stream().mapToLong(i -> i).max().getAsLong();
+        long min = sumByChar.values().stream().mapToLong(i -> i).min().getAsLong();
+        return max - min;
     }
 
+    static Map<String, Long> doStep(Map<String, Long> pairs, Map<String, String> pairToCharRules) {
+        return pairs.entrySet().stream()
+                // explode one pair to two pairs by the rules
+                .flatMap(pair -> pairToTwoPairs(pair.getKey(), pair.getValue(), pairToCharRules))
+                // sum the counts by pair
+                .collect(Collectors.groupingBy(pair -> pair.getKey(), Collectors.summingLong(pair -> pair.getValue())));
+    }
 
-    static Map<Character,Long> countCharsForPair(char[] pair, boolean lastPair, int depth) {
-        if (depth == 0) {
-            // For last pair count both chars, for others only the first char
-            return lastPair ? Map.of(pair[0], 1l, pair[1], 1l) : Map.of(pair[0], 1l);
-        }
-        return cache.computeIfAbsent(new String(pair)+lastPair+depth, k -> {
-            // Calculate pair recursively if not calculate already
-            char pairToChar = pairToCharacterRule.get(new String(pair));
-            char[] pair1 =  { pair[0],  pairToChar };
-            char[] pair2 = { pairToChar,  pair[1]};
-            var p1Count = countCharsForPair(pair1, false, depth-1);
-            var p2Count = countCharsForPair(pair2, lastPair, depth-1);
-            return mergeCountsByCharacter(p1Count, p2Count);
+    static Stream<Map.Entry<String, Long>> pairToTwoPairs(String pair, long count, Map<String, String> pairToCharRules) {
+        String pairToChar = pairToCharRules.get(pair);
+        String pair1 = pair.charAt(0) + pairToChar;
+        String pair2 = pairToChar + pair.charAt(1);
+        return Stream.of(Map.entry(pair1, count), Map.entry(pair2, count));
+    }
+
+    static Map<Character, Long> countFirstCharsOfPairs(Map<String, Long> pairs) {
+        return pairs.entrySet().stream()
+                .collect(Collectors.groupingBy(e -> e.getKey().charAt(0),
+                        Collectors.summingLong(e -> e.getValue())));
+    }
+
+    static Map<String, Long> countPairs(String str) {
+        return stringAsPairs(str).stream()
+                .collect(Collectors.groupingBy(pair -> pair, Collectors.counting()));
+    }
+
+    static List<String> stringAsPairs(String template) {
+        List<String> pairs = new ArrayList<>();
+        template.chars().reduce((a, b) -> {
+            pairs.add(new String(new char[] { (char) a, (char) b }));
+            return b;
         });
-    }
-
-    static Map<Character,Long> mergeCountsByCharacter(Map<Character,Long> a,Map<Character,Long> b) {
-        return Stream.concat(a.entrySet().stream(), b.entrySet().stream())
-            .collect(Collectors.groupingBy(e -> e.getKey(), Collectors.summingLong(e -> e.getValue())));
-    }    
-    
-    static List<char[]> stringAsCharPairs(String template) {
-        List<char[]> pairs = new ArrayList<>();
-        template.chars().reduce((a,b) -> { pairs.add(new char[]{(char)a, (char)b}); return b; });
         return pairs;
     }
 }
-
-
