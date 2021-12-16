1f20b9103bca8a7512495ca8ffd52ad2244908cc
Pekka Vainio <pekka.vainio@iki.fi>
2021-12-16 14:11:19 +0200

    Day 16

---
+++ 2021/go/d16/main.go
@@ -0,0 +1,192 @@
+package main
+
+import (
+	"aoc2021/util"
+	"bytes"
+	"fmt"
+	"math"
+	"strconv"
+)
+
+type packet interface {
+	head() packetHeader
+	version() int
+	content() int64
+}
+
+type packetHeader struct {
+	version, typeId int
+}
+
+type literalPacket struct {
+	header packetHeader
+	value  int
+}
+
+type operatorPacket struct {
+	header  packetHeader
+	packets []packet
+}
+
+// Advent of Code (AOC) 2021 Day 16
+func main() {
+
+	var data string
+	util.ReadFile("../input/16a.txt", func(line string) {
+		data = asBits(line)
+	})
+
+	in := bytes.NewBufferString(data)
+
+	p := parsePacket(in)
+
+	fmt.Printf("part 1: %v\n", p.version())
+	fmt.Printf("part 2: %v\n", p.content())
+}
+
+func parsePacket(in *bytes.Buffer) packet {
+	header := packetHeader{version: getBits(in, 3), typeId: getBits(in, 3)}
+	switch header.typeId {
+	case 4:
+		return parseLiteral(header, in)
+	default:
+		return parseOperator(header, in)
+	}
+}
+
+func parseLiteral(header packetHeader, in *bytes.Buffer) packet {
+	var value int = 0
+	groupPrefix := 0
+	for ok := true; ok; ok = groupPrefix == 1 {
+		groupPrefix = getBits(in, 1)
+		value = (value << 4) + getBits(in, 4)
+	}
+	return literalPacket{header: header, value: value}
+}
+
+func parseOperator(header packetHeader, in *bytes.Buffer) packet {
+	lengthType := getBits(in, 1)
+	if lengthType == 0 {
+		return parsePacketsLength(header, in)
+	} else {
+		return parsePacketsNumber(header, in)
+	}
+}
+
+func parsePacketsLength(header packetHeader, in *bytes.Buffer) packet {
+	length := getBits(in, 15)
+	startLen := in.Len()
+	packets := []packet{}
+	for startLen-in.Len() < length {
+		packets = append(packets, parsePacket(in))
+	}
+	return operatorPacket{header: header, packets: packets}
+}
+
+func parsePacketsNumber(header packetHeader, in *bytes.Buffer) packet {
+	number := getBits(in, 11)
+	packets := []packet{}
+	for i := 0; i < number; i++ {
+		packets = append(packets, parsePacket(in))
+	}
+	return operatorPacket{header: header, packets: packets}
+}
+
+func (p literalPacket) head() packetHeader {
+	return p.header
+}
+
+func (p operatorPacket) head() packetHeader {
+	return p.header
+}
+
+func (p literalPacket) version() int {
+	return p.header.version
+}
+
+func (p operatorPacket) version() int {
+	subVersion := 0
+	for _, p := range p.packets {
+		subVersion += p.version()
+	}
+	return p.header.version + subVersion
+}
+
+func (p literalPacket) content() int64 {
+	return int64(p.value)
+}
+
+func (p operatorPacket) content() int64 {
+	switch p.header.typeId {
+	case 0:
+		return reduce(p.packets, 0, func(a int64, b int64) int64 { return a + b })
+	case 1:
+		return reduce(p.packets, 1, func(a int64, b int64) int64 { return a * b })
+	case 2:
+		return reduce(p.packets, math.MaxInt64, func(a int64, b int64) int64 { return min(a, b) })
+	case 3:
+		return reduce(p.packets, math.MinInt64, func(a int64, b int64) int64 { return max(a, b) })
+	case 5:
+		return compare(p.packets, func(a int64, b int64) bool { return a > b })
+	case 6:
+		return compare(p.packets, func(a int64, b int64) bool { return a < b })
+	case 7:
+		return compare(p.packets, func(a int64, b int64) bool { return a == b })
+	default:
+		return -1
+	}
+}
+
+func compare(packets []packet, fn func(a int64, b int64) bool) int64 {
+	if fn(packets[0].content(), packets[1].content()) {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func reduce(packets []packet, value int64, fn func(a int64, b int64) int64) int64 {
+	for _, p := range packets {
+		value = fn(value, p.content())
+	}
+	return value
+}
+
+func min(a int64, b int64) int64 {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func max(a int64, b int64) int64 {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func getBits(in *bytes.Buffer, len int) int {
+	bits := string(in.Next(len))
+	val, _ := strconv.ParseInt(bits, 2, 32)
+	return int(val)
+}
+
+func asBits(hex string) string {
+	var result string
+	for _, c := range hex {
+		result += hexToBits(c)
+	}
+	return result
+}
+
+func hexToBits(hex rune) string {
+	v, _ := strconv.ParseInt(string(hex), 16, 8)
+	res := strconv.FormatInt(v, 2)
+	for len(res) < 4 {
+		res = "0" + res
+	}
+	return res
+}
---
+++ 2021/java/src/main/java/aoc2021/D16.java
@@ -0,0 +1,120 @@
+package aoc2021;
+
+import java.io.IOException;
+import java.nio.CharBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HexFormat;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * Advent of Code (AOC) 2021 Day 15
+ */
+public class D16 {
+
+    interface Packet {
+        int version();
+        int type();
+        long value();
+    }
+
+    record LiteralPacket(int version, int type, long value) implements Packet{}
+
+    record OperatorPacket(int version, int type, List<Packet> packets) implements Packet {
+        public long value() {
+            return switch (type) {
+                case 0 -> packets.stream().mapToLong(p -> p.value()).sum();
+                case 1 -> packets.stream().mapToLong(p -> p.value()).reduce(1, (a,b) -> a*b); 
+                case 2 -> packets.stream().mapToLong(p -> p.value()).min().getAsLong();
+                case 3 -> packets.stream().mapToLong(p -> p.value()).max().getAsLong();
+                case 5 -> packets.get(0).value() > packets.get(1).value() ? 1l : 0l;
+                case 6 -> packets.get(0).value() < packets.get(1).value() ? 1l : 0l;
+                case 7 -> packets.get(0).value() == packets.get(1).value() ? 1l : 0l;
+                default -> throw new IllegalStateException("invalid packet type " + type);
+            };
+        }
+
+        public int version() {
+            return version + packets.stream().mapToInt(p -> p.version()).sum();
+        }
+    }
+
+    public static void main(String... args) throws IOException {
+        String data = asBits(Files.lines(Path.of("../input/16a.txt")).findFirst().get());
+
+        Packet p = parsePacket(CharBuffer.wrap(data));
+
+        System.out.printf("part 1: %d\n", p.version());
+        System.out.printf("part 2: %d\n", p.value());
+    }
+
+    static Packet parsePacket(CharBuffer in) {
+        int version = getBits(in, 3);
+        int type = getBits(in, 3);
+        return switch(type) {
+            case 4 -> parseLiteralPacket(version, type, in);
+            default -> parseOperatorPacket(version, type, in);
+        };
+    }
+
+    static Packet parseOperatorPacket(int version, int type, CharBuffer in) {
+        int lengthType = getBits(in, 1);
+        if (lengthType == 0) {
+            return parsePacketsLength(version, type, in);
+        } else {
+            return parsePacketsNumber(version, type, in);
+        }
+    }
+
+    static Packet parsePacketsLength(int version, int type, CharBuffer in) {
+        int length = getBits(in, 15);
+        int start = in.position();
+        // parse packets until length consumed
+        var packets = Stream.iterate(0, i -> in.position() - start < length, (a) -> 0)
+            .map(i -> parsePacket(in))
+            .toList();
+        return new OperatorPacket(version, type, packets);
+    }
+
+    static Packet parsePacketsNumber(int version, int type, CharBuffer in) {
+        int number = getBits(in, 11);
+        var packets = IntStream.range(0, number).mapToObj(i -> parsePacket(in)).toList();
+        return new OperatorPacket(version, type, packets);
+    }
+
+    static Packet parseLiteralPacket(int version, int type, CharBuffer in) {
+        long value = 0;
+        int groupPrefix;
+        do {
+            groupPrefix = getBits(in, 1);
+            value = (value << 4) + getBits(in, 4); // add 4 bits to literal
+        } while (groupPrefix > 0); // group is prefixed with 1 until last group is not
+        return new LiteralPacket(version, type, value);
+    }
+
+    static int getBits(CharBuffer in, int len) {
+        if (in.remaining() < len) {
+            return -1;
+        }
+        String bits = in.subSequence(0, len).toString();
+        in.position(in.position()+len);
+        return Integer.parseInt(bits, 2);
+    }
+
+    static String asBits(String hexString) {
+        return hexString.chars()
+            .map(c -> HexFormat.fromHexDigit(c))
+            .mapToObj(h -> asBits(h, 4)).collect(Collectors.joining(""));
+    }
+
+    static String asBits(int value, int len) {
+        String str = Integer.toString(value, 2);
+        while (str.length() < len) {
+            str = "0" + str;
+        }
+        return str;
+    }
+}
